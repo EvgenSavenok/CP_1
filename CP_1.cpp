7bfbdfec2d0d21ac48bd41f753e5a2eaa1d08436
EvgenSavenok <eugen.savenok@gmail.com>
2024-09-07 13:00:02 +0300

    Feat: test scanNextt

--- CP_1.cpp
+++ CP_1.cpp
@@ -2,6 +2,7 @@
 #include <commctrl.h>
 #include <string>
 #include <vector>
+#include <codecvt>
 
 #pragma comment(lib, "Comctl32.lib")
 
@@ -76,7 +77,7 @@ HWND CreateStartBtn(HWND hwndParent)
         L"BUTTON",      
         L"Sort",  
         WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON,  
-         windowWidth - 120, windowHeight - 100, 60, 30,
+         windowWidth - 120, windowHeight - 100, 60, 50,
         hwndParent,     
         (HMENU)IDC_SEARCHBTN,       
         (HINSTANCE)GetWindowLongPtr(hwndParent, GWLP_HINSTANCE), 
@@ -85,41 +86,49 @@ HWND CreateStartBtn(HWND hwndParent)
     return hwndButton;
 }
 
-HWND AllocateHeaderTableMemory(HWND hwndListView, const int WINDOW_WIDTH)
-{
-    for (int i = 0; i < 2; ++i)
-    {
-        LVCOLUMN lvc;
-        ZeroMemory(&lvc, sizeof(LVCOLUMN));
-        lvc.mask = LVCF_TEXT | LVCF_WIDTH;
-        lvc.cx = WINDOW_WIDTH / 2;      
-        ListView_InsertColumn(hwndListView, i, &lvc);  
-    }
-
-    std::vector<std::vector<std::wstring>> rows = {
-        { L"Directory", L"Result" }
-    };
-
-    for (int i = 0; i < rows.size(); ++i)
-    {
-        LVITEM lvItem;
-        ZeroMemory(&lvItem, sizeof(LVITEM));
-
-        lvItem.mask = LVIF_TEXT;
-        lvItem.iItem = i; 
-        lvItem.iSubItem = 0; 
-
-        lvItem.pszText = const_cast<LPWSTR>(rows[i][0].c_str());
-        ListView_InsertItem(hwndListView, &lvItem);
-
-        ListView_SetItemText(hwndListView, i, 1, const_cast<LPWSTR>(rows[i][1].c_str()));
-    }
-    return hwndListView;
-}
+//HWND AllocateHeaderTableMemory(HWND hwndListView, const int WINDOW_WIDTH)
+//{
+//    for (int i = 0; i < 2; ++i)
+//    {
+//        LVCOLUMN lvc;
+//        ZeroMemory(&lvc, sizeof(LVCOLUMN));
+//        lvc.mask = LVCF_TEXT | LVCF_WIDTH;
+//        lvc.cx = WINDOW_WIDTH / 2;      
+//        ListView_InsertColumn(hwndListView, i, &lvc);  
+//    }
+//
+//    std::vector<std::vector<std::wstring>> rows = {
+//        { L"C:\\*", L"Result" }
+//    };
+//
+//    for (int i = 0; i < rows.size(); ++i)
+//    {
+//        LVITEM lvItem;
+//        ZeroMemory(&lvItem, sizeof(LVITEM));
+//
+//        lvItem.mask = LVIF_TEXT;
+//        lvItem.iItem = i; 
+//        lvItem.iSubItem = 0; 
+//
+//        lvItem.pszText = const_cast<LPWSTR>(rows[i][0].c_str());
+//        ListView_InsertItem(hwndListView, &lvItem);
+//
+//        ListView_SetItemText(hwndListView, i, 1, const_cast<LPWSTR>(rows[i][1].c_str()));
+//    }
+//    return hwndListView;
+//}
 
 HWND AllocateDirectoriesTableMemory(HWND hwndListView, const int WINDOW_WIDTH)
 {
     std::vector<std::vector<std::wstring>> rows = {
+        { L"1", L"C:\hentai", L"13 kb", L"1", L"C:\hentai", L"15 kb" },
+        { L"1", L"C:\hentai", L"13 kb", L"1", L"C:\hentai", L"15 kb" },
+        { L"1", L"C:\hentai", L"13 kb", L"1", L"C:\hentai", L"15 kb" },
+        { L"1", L"C:\hentai", L"13 kb", L"1", L"C:\hentai", L"15 kb" },
+        { L"1", L"C:\hentai", L"13 kb", L"1", L"C:\hentai", L"15 kb" },
+        { L"1", L"C:\hentai", L"13 kb", L"1", L"C:\hentai", L"15 kb" },
+        { L"1", L"C:\hentai", L"13 kb", L"1", L"C:\hentai", L"15 kb" },
+        { L"1", L"C:\hentai", L"13 kb", L"1", L"C:\hentai", L"15 kb" },
         { L"1", L"C:\hentai", L"13 kb", L"1", L"C:\hentai", L"15 kb" },
         { L"2", L"C:\hentai", L"19 kb", L"2", L"C:\hentai", L"11 kb" }
     };
@@ -154,6 +163,20 @@ HWND AllocateDirectoriesTableMemory(HWND hwndListView, const int WINDOW_WIDTH)
     }
 
     return hwndListView;
+}
+
+HWND CreateEditBox(HWND hwndParent)
+{
+    int xpos = 8;            
+    int ypos = 10;            
+    int nwidth = 200;         
+    int nheight = 25;         
+
+    HWND hwndEditBox = CreateWindowEx(WS_EX_CLIENTEDGE, L"EDIT", L"C:\\*",
+        WS_CHILD | WS_VISIBLE | WS_BORDER | ES_LEFT,
+        xpos, ypos, nwidth, nheight, hwndParent, (HMENU)1001, GetModuleHandle(NULL), NULL);
+
+    return hwndEditBox;
 }
 
 
@@ -191,10 +214,9 @@ void CreateTables(HWND hwndParent)
     int windowWidth = CalculateWindowWidth(hwndParent);
     int windowHeight = CalculateWindowHeight(hwndParent);
 
-    HWND hwndListView = CreateTable(hwndParent, windowWidth, 20, 8, 0, 1001, true);
-    AllocateHeaderTableMemory(hwndListView, windowWidth);
-    hwndListView = CreateTable(hwndParent, windowWidth, windowHeight - 130,
-        8, 20, 1002, false);
+    //AllocateHeaderTableMemory(hwndListView, windowWidth);
+    HWND hwndListView = CreateTable(hwndParent, windowWidth, windowHeight - 170,
+        8, 50, 1002, false);
     AllocateDirectoriesTableMemory(hwndListView, windowWidth);
 }
 
@@ -243,18 +265,91 @@ bool ProcessNotify(LPARAM lParam)
     return false;
 }
 
+TCHAR* OnButtonClick(HWND hwndParent) {
+    HWND hWndEdit = GetDlgItem(hwndParent, 1001);
+    int length = GetWindowTextLengthW(hWndEdit) + 1;
+    TCHAR* buffer = new TCHAR[length];
+    if (buffer)
+    {
+        GetDlgItemTextW(hwndParent, 1001, (LPWSTR)buffer, length);
+    }
+    return buffer;
+}
+
+#define MASK_FOUND L"*"
+#define SIZE_BUF 260
+
+struct FileInfo {
+    std::string fileName;
+    LARGE_INTEGER fileSize;
+};
+
+void ScanNext(const wchar_t* directory, std::vector<FileInfo>& files)
+{
+    size_t len = wcslen(directory) + 1; 
+    wchar_t* tmpDir = new wchar_t[len];
+    wcscpy_s(tmpDir, len, directory); 
+    std::wstring searchPath;
+    searchPath += std::wstring(tmpDir);
+    WIN32_FIND_DATA findFileData = { 0 };
+    HANDLE hFind = FindFirstFileW(searchPath.c_str(), &findFileData);
+    if (hFind == INVALID_HANDLE_VALUE)
+    {
+        delete[] tmpDir;
+        return;
+    }
+    wchar_t wsInput[] = L"C:\\*";
+    if (wcscmp(tmpDir, L"C:\\*") == 0)
+        tmpDir[wcslen(tmpDir) - 2] = '\0';
+    do {
+        std::wstring fileName = findFileData.cFileName;
+        std::wstring fullPath;
+        fullPath.append(tmpDir);
+        fullPath.append(L"\\");
+        fullPath.append(fileName);
+        delete[] tmpDir;
+
+        if (fileName == L"." || fileName == L"..") {
+            continue;
+        }
+
+        if (findFileData.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY &&
+            !(findFileData.dwFileAttributes & FILE_ATTRIBUTE_SYSTEM))
+        {
+            ScanNext(fullPath.c_str(), files);
+        }
+        else 
+        {            
+            FileInfo fileInfo;
+            std::wstring_convert<std::codecvt_utf8<wchar_t>, wchar_t> converter;
+            fileInfo.fileName = converter.to_bytes(fullPath);
+            fileInfo.fileSize.LowPart = findFileData.nFileSizeLow;
+            fileInfo.fileSize.HighPart = findFileData.nFileSizeHigh;
+            files.push_back(fileInfo);
+        }
+    } while (FindNextFileW(hFind, &findFileData) != 0);
+    FindClose(hFind);
+    delete[] tmpDir;
+}
+
+void StartScanning(HWND hwnd)
+{
+    wchar_t* directory = OnButtonClick(hwnd);
+    std::vector<FileInfo> files;
+    ScanNext(directory, files);
+}
+
 LRESULT CALLBACK WndProc(HWND hwnd/*дескриптор окна*/, UINT uMsg/*идентификатор 
     сообщения*/, WPARAM wParam, LPARAM lParam)
 {
-    HWND hwndListView = {};
     switch (uMsg) 
     {
     case WM_COMMAND:
     {
         int wmId = LOWORD(wParam);
-        if (wmId == IDC_SEARCHBTN)
+        if (LOWORD(wParam) == IDC_SEARCHBTN) 
         {
-            //Start sorting...
+            StartScanning(hwnd);
         }
         break;
     }
@@ -264,6 +359,7 @@ LRESULT CALLBACK WndProc(HWND hwnd/*дескриптор окна*/, UINT uMsg/*
     }
     case WM_CREATE:
         CreateTables(hwnd);
+        CreateEditBox(hwnd);
         CreateStartBtn(hwnd);   
         break;
     case WM_CLOSE:
